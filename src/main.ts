ff996c59f0ef789099743a0032e6358ae1190652
Collin Pfeifer <collinpfeifer@Icloud.com>
2024-05-16 10:35:31 -0400

    trying out persisted operations to see if it works and also for security reasons

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,29 @@ import { createServer } from 'http';
 import { createYoga } from 'graphql-yoga';
 import { createContext } from './context';
 import { schema } from './schema';
+import { usePersistedOperations } from '@graphql-yoga/plugin-persisted-operations';
+import { readFileSync } from 'node:fs';
 
 function main() {
+  const persistedOperations = JSON.parse(
+    readFileSync('./persistedOperations.json', 'utf-8')
+  );
   const yoga = createYoga({
     schema,
     context: createContext,
     logging: 'debug',
     healthCheckEndpoint: '/health',
+    plugins: [
+      usePersistedOperations({
+        getPersistedOperation(sha256Hash: string) {
+          return persistedOperations[sha256Hash];
+        },
+      }),
+    ],
   });
   const server = createServer(yoga);
   server.listen(process.env.PORT, () => {
-    console.info(
-      `Server is running on port ${process.env.PORT}`
-    );
+    console.info(`Server is running on port ${process.env.PORT}`);
   });
 }
 
