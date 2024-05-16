cecedf7d789f18267f8f39dd4a7e44a0834a0be1
Collin Pfeifer <collinpfeifer@Icloud.com>
2024-05-16 11:51:29 -0400

    persisted operations too much work rn

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,26 @@ import { createServer } from 'http';
 import { createYoga } from 'graphql-yoga';
 import { createContext } from './context';
 import { schema } from './schema';
-import { usePersistedOperations } from '@graphql-yoga/plugin-persisted-operations';
+// import { usePersistedOperations } from '@graphql-yoga/plugin-persisted-operations';
 import { readFileSync } from 'node:fs';
 
 function main() {
-  const persistedOperations = JSON.parse(
-    readFileSync('src/persistedOperations.json', 'utf-8')
-  );
+  // const persistedOperations = JSON.parse(
+  //   readFileSync('src/persistedOperations.json', 'utf-8')
+  // );
 
   const yoga = createYoga({
     schema,
     context: createContext,
     logging: 'debug',
     healthCheckEndpoint: '/health',
-    plugins: [
-      usePersistedOperations({
-        getPersistedOperation(sha256Hash: string) {
-          return persistedOperations[sha256Hash];
-        },
-      }),
-    ],
+    // plugins: [
+    //   usePersistedOperations({
+    //     getPersistedOperation(sha256Hash: string) {
+    //       return persistedOperations[sha256Hash];
+    //     },
+    //   }),
+    // ],
   });
   const server = createServer(yoga);
   server.listen(process.env.PORT, () => {
